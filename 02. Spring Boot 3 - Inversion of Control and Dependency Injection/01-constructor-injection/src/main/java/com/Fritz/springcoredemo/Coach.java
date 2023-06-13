64dc2fc7d35a2f094756f29fde53e3285e9ee08a
Florian-Cristinel Ianculescu <florian.ianculescu@gmail.com>
2023-06-13 14:39:33 +0300

    Add files via upload

---
+++ 02. Spring Boot 3 - Inversion of Control and Dependency Injection/01-constructor-injection/src/main/java/com/Fritz/springcoredemo/Coach.java
@@ -0,0 +1,6 @@
+package com.Fritz.springcoredemo;
+
+public interface Coach {
+
+    String getDailyWorkout();
+}
---
+++ 02. Spring Boot 3 - Inversion of Control and Dependency Injection/02-component-scanning/src/main/java/com/Fritz/springcoredemo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.Fritz.springcoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+@SpringBootApplication(
+		scanBasePackages = {"com.Fritz.springcoredemo",
+							"com.Fritz.util"})
+*/
+@SpringBootApplication
+public class  DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
