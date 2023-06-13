81ddfc7eb2b3e126f1e0b3df06f4a8af951398d3
Florian-Cristinel Ianculescu <florian.ianculescu@gmail.com>
2023-06-13 11:48:34 +0300

    Add files via upload

---
+++ 9. Spring Boot 3 - JPA-Hibernate Advanced Mappings/03-jpa-one-to-many/src/main/java/com/springcourse/cruddemo/dao/AppDAO.java
@@ -0,0 +1,24 @@
+package com.springcourse.cruddemo.dao;
+
+import com.springcourse.cruddemo.entity.Course;
+import com.springcourse.cruddemo.entity.Instructor;
+import com.springcourse.cruddemo.entity.InstructorDetail;
+
+import java.util.List;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+
+    InstructorDetail findInstructorDetailById(int theId);
+
+    void deleteInstructorDetailById(int theId);
+
+    List<Course> findCoursesByInstructorId(int theId);
+
+    Instructor findInstructorByIdJoinFetch(int theId);
+}
