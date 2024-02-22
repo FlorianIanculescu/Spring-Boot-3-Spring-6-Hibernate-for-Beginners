f92776e0bf37c12d7de204cb0c80e348f7b5ea9e
Florian-Cristinel Ianculescu <florian.ianculescu@gmail.com>
2024-02-22 14:18:30 +0200

    Add files via upload

---
+++ 02. Spring Core - Inversion of Control and Dependency Injection/09-java-config-bean/src/main/java/com/Fritz/springcoredemo/config/SportConfig.java
@@ -0,0 +1,15 @@
+package com.Fritz.springcoredemo.config;
+
+import com.Fritz.springcoredemo.common.Coach;
+import com.Fritz.springcoredemo.common.SwimCoach;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SportConfig {
+
+    @Bean("aquatic")
+    public Coach swimCoach() {
+        return new SwimCoach();
+    }
+}
