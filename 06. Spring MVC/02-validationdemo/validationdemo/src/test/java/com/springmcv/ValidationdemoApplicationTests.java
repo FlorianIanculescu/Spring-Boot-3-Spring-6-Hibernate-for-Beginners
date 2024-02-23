89725e6179a8062d4778f8f092680e4a10c9cd33
Florian-Cristinel Ianculescu <florian.ianculescu@gmail.com>
2024-02-23 09:10:46 +0200

    Add files via upload

---
+++ 06. Spring MVC/02-validationdemo/validationdemo/src/test/java/com/springmcv/ValidationdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmcv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
